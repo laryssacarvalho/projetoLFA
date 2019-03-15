db983ef64cee83d077e107a6a409a006e1fca4f9
Daniel Braga <dan.b.v.r05@gmail.com>
2019-03-15 10:00:00 -0300

    Adicionando Classe Grammar e Refatorando utilizando funções

---
+++ Projeto1/Grammar.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Projeto1
+{
+    class Grammar
+    {
+        public string[] Variaveis { get; set; }
+        
+        public string[] Alfabeto { get; set; }
+
+        public string Inicial { get; set; }
+
+        public List<string> P0  { get; set; }
+
+        public List<string> P1 { get; set; }
+
+        public List<string> Aux { get; set; }
+
+        public Grammar()
+        {
+            Variaveis = new string[] { "" };
+            Inicial = "";
+            P0 = new List<string>();
+            P1 = new List<string>();
+            Aux = new List<string>();
+        } 
+        
+        public void GetVariaveis()
+        {
+            bool repeat = true;
+
+            while (repeat)
+            {
+                Console.WriteLine("Insira os dados da Linguagem: \n");
+                Console.WriteLine("Digite as variáveis separadas por vírgula. Ex: X, Y, Z");
+                Variaveis = Console.ReadLine().Replace(" ", string.Empty).Split(',');
+
+                repeat = CheckVariaveis();
+            }
+        }
+
+        public bool CheckVariaveis()
+        {
+            if (Variaveis.Length <= 0 || Variaveis[0] == "")
+            {
+                Console.WriteLine("\n\tERRO: Por favor, digite ao menos uma variável!\n");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void GetAlfabeto()
+        {
+            bool repeat = true;
+
+            while (repeat)
+            {
+                Console.WriteLine("\nDigite o Alfabeto separado por vírgula. Ex: a, b, c");
+                Alfabeto = Console.ReadLine().Replace(" ", string.Empty).Split(',');
+                repeat = CheckAlfabeto();
+            }
+        }
+
+        public bool CheckAlfabeto()
+        {
+            if (Alfabeto.Length <= 0 || Alfabeto[0] == "")
+            {
+                Console.WriteLine("\n\tERRO: Por favor, digite ao menos um item para o Alfabeto!");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        public void GetInicial()
+        {
+            bool repeat = true;
+
+            while (repeat)
+            {
+                Console.WriteLine("\nDigite a variável Inicial");
+                Inicial = Console.ReadLine().Replace(" ", string.Empty);
+                repeat = CheckInicial();
+            }
+        }
+
+        public bool CheckInicial()
+        {
+            if (string.IsNullOrEmpty(Inicial))
+            {
+                Console.WriteLine("\n\tERRO: Por favor, insira a variável Inicial!");
+                return true;
+            }
+            else if (!Variaveis.Contains(Inicial))
+            {
+                Console.WriteLine("\n\tERRO: Por favor, a variável Inicial deve ser uma das variáveis!");
+                Console.WriteLine("\nRegras: " + string.Join(" ", Variaveis));
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void GetRegrasProducao()
+        {
+            bool repeat = true;
+
+            while (repeat)
+            {
+                Console.WriteLine("\nDigite as regras de produção seguindo o exemplo: S>AB, A>aA, Bb>bB");
+                Aux = Console.ReadLine().Replace(" ", string.Empty).Split(',').ToList();
+
+                foreach (string s in Aux)
+                {
+                    if (s.Length > 2)
+                    {
+                        P0.Add(s.Split('>')[0]);
+                        P1.Add(s.Split('>')[1]);
+                    }
+                }
+
+                repeat = CheckRegrasProducao();
+            }
+        }
+
+        public bool CheckRegrasProducao()
+        {
+            if (P0.Count == 0)
+            {
+                Console.WriteLine("\n\tERRO: Por favor, insira pelo menos uma regra de produção!");
+                P0 = new List<string>();
+                P1 = new List<string>();
+                return true;
+            }
+            else if (!P0.Contains(Inicial))
+            {
+                Console.WriteLine("\n\tERRO: Por favor, ao menos uma regra deve conter a variável Inicial!");
+                P0 = new List<string>();
+                P1 = new List<string>();
+                return true;
+            }
+            else
+            {
+                return false;
+            }            
+        }
+    }
+}
--- Projeto1/Program.cs
+++ Projeto1/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Projeto1
 {
@@ -11,167 +8,41 @@ namespace Projeto1
     {
         static void Main(string[] args)
         {
-            bool repeat = true, repeatProg = true;
+            bool repeat = true, repeatProg = true, newG = true;
+
+            Grammar g = new Grammar();
 
             while (repeatProg)
-            {
-                string[] variaveis = { "" };
-                string[] alfabeto;
-                string inicial = "", palavra = "", repetir = "";
-                List<string> p0 = new List<string>();
-                List<string> p1 = new List<string>();
-                List<string> aux = new List<string>();
-                List<int> entrada = new List<int>();
+            {                
+                if (newG)
+                {
+                    Console.Clear();
+
+                    g = new Grammar();
+
+                    g.GetVariaveis();
+                    g.GetAlfabeto();
+                    g.GetInicial();
+                    g.GetRegrasProducao();
+
+                    newG = false;
+                }
+
+                string palavra = "";
                 int escolha = 0;
+                List<int> entrada = new List<int>();
 
-                repeat = true;
                 Console.Clear();
-                while (repeat)
-                {
-                    Console.WriteLine("Insira os dados da Linguagem: \n");
-                    Console.WriteLine("Digite as variáveis separadas por vírgula. Ex: X, Y, Z");
-                    variaveis = Console.ReadLine().Replace(" ", string.Empty).Split(',');
 
-                    if (variaveis.Length <= 0 || variaveis[0] == "")
-                    {
-                        Console.WriteLine("\n\tERRO: Por favor, digite ao menos uma variável!\n");
-                    }
-                    else
-                    {
-                        repeat = false;
-                    }
-                }
-
-                repeat = true;
-
-                while (repeat)
-                {
-                    Console.WriteLine("\nDigite o alfabeto separado por vírgula. Ex: a, b, c");
-                    alfabeto = Console.ReadLine().Replace(" ", string.Empty).Split(',');
-
-                    if (alfabeto.Length <= 0 || alfabeto[0] == "")
-                    {
-                        Console.WriteLine("\n\tERRO: Por favor, digite ao menos um item para o alfabeto!");
-                    }
-                    else
-                    {
-                        repeat = false;
-                    }
-                }
-
-                repeat = true;
-
-                while (repeat)
-                {
-                    Console.WriteLine("\nDigite a variável inicial");
-                    inicial = Console.ReadLine().Replace(" ", string.Empty);
-
-                    if (String.IsNullOrEmpty(inicial))
-                    {
-                        Console.WriteLine("\n\tERRO: Por favor, insira a variável inicial!");
-                    }
-                    else if (!variaveis.Contains(inicial))
-                    {
-                        Console.WriteLine("\n\tERRO: Por favor, a variável inicial deve ser uma das variáveis!");
-                        variaveis.ToList().ForEach(Console.Write);
-                    }
-                    else
-                    {
-                        repeat = false;
-                    }
-                }
-
-                repeat = true;
-
-                while (repeat)
-                {
-                    Console.WriteLine("\nDigite as regras de produção seguindo o exemplo: S>AB, A>aA, Bb>bB");
-                    aux = Console.ReadLine().Replace(" ", string.Empty).Split(',').ToList();
-
-                    foreach (string s in aux)
-                    {
-                        if (s.Length > 2)
-                        {
-                            p0.Add(s.Split('>')[0]);
-                            p1.Add(s.Split('>')[1]);
-                        }
-                    }
-                    if (p0.Count == 0)
-                    {
-                        Console.WriteLine("\n\tERRO: Por favor, insira pelo menos uma regra de produção!");
-                        p0 = new List<string>();
-                        p1 = new List<string>();
-                    }
-                    else if (!p0.Contains(inicial))
-                    {
-                        Console.WriteLine("\n\tERRO: Por favor, ao menos uma regra deve conter a variável inicial!");
-                        p0 = new List<string>();
-                        p1 = new List<string>();
-                    }
-                    else
-                    {
-                        repeat = false;
-                    }
-                }
-
-                repeat = true;
-
-                while (repeat)
-                {
-                    Console.WriteLine("\nSelecione uma das opções abaixo:\n\n1 - Decifrar palavra\n2 - Derivar palavra");
-                    escolha = System.Convert.ToInt32(Console.ReadLine());
-
-                    if (escolha != 1 && escolha != 2)
-                    {
-                        Console.WriteLine("\n\tERRO: Opção Inválida!");
-                        repeat = true;
-                    }
-                    else
-                    {
-                        repeat = false;
-                    }
-                }
-
-                repeat = true;
+                escolha = SelecionaOpcoes();                
 
                 if (escolha == 1)
                 {
-                    while (repeat)
-                    {
-                        Console.WriteLine("\nDigite os valores de entrada separados por vírgula. Ex: 1, 12, 7");
-                        aux = Console.ReadLine().Replace(" ", string.Empty).Split(',').ToList();
+                    GetEntradas(g, entrada);
 
-                        foreach (string s in aux)
-                        {
-                            if (s != "")
-                            {
-                                entrada.Add(Convert.ToInt32(s));
-                            }
-                        }
+                    string saida = decifrarPalavra(g.P0, g.P1, entrada);
 
-                        if (entrada.Count <= 0)
-                        {
-                            Console.WriteLine("\n\tERRO: Por favor, digite ao menos um valor de entrada!");
-                            entrada = new List<int>();
-                        }
-                        else if (entrada.Max() > p0.Count)
-                        {
-                            Console.WriteLine("\n\tERRO: Por favor, digite apenas entradas válidas!");
-                            entrada = new List<int>();
-                        }
-                        else if (p0[entrada[0] - 1] != inicial)
-                        {
-                            Console.WriteLine("\n\tERRO: Por favor, a primeira entrada deve possuir a variável inicial!");
-                            entrada = new List<int>();
-                        }
-                        else
-                        {
-                            repeat = false;
-                        }
-                    }
-                    string saida = decifrarPalavra(p0, p1, entrada);
-
-                    if (variaveis.Any(c => saida.Contains(c)))
+                    if (g.Variaveis.Any(c => saida.Contains(c)))
                     {
                         Console.WriteLine($"\nNão foi possível decifrar a palavra. Saída: {saida}");
                     }
@@ -183,29 +54,16 @@ namespace Projeto1
 
                 if (escolha == 2)
                 {
-                    while (repeat)
-                    {
-                        Console.WriteLine("\nDigite a palavra a ser derivada.");
-                        palavra = Console.ReadLine();
-
-                        if (palavra == "")
-                        {
-                            Console.WriteLine("\n\tERRO: Por favor, digite uma palavra!");
-                        }
-                        else
-                        {
-                            repeat = false;
-                        }
-                    }
+                    palavra = GetPalavra();
 
                     List<int> regras = new List<int>();
 
-                    Node saida = derivarPalavra(p0, p1, inicial, palavra);
+                    Node saida = derivarPalavra(g.P0, g.P1, g.Inicial, palavra);
 
                     if (saida != null)
                     {
                         Node n = saida;
-                        while (n.palavra != inicial)
+                        while (n.palavra != g.Inicial)
                         {
                             regras.Add(n.regra + 1);
                             n = n.parent;
@@ -216,27 +74,11 @@ namespace Projeto1
                     }
                 }
 
-                repeat = true;
+                repeatProg = NovaOperacao();
 
-                while (repeat)
+                if (repeatProg)
                 {
-                    Console.WriteLine($"\nDeseja fazer uma nova operação? (S/N)");
-                    repetir = Console.ReadLine();
-
-                    if (repetir == "S" || repetir == "s")
-                    {
-                        repeatProg = true;
-                        repeat = false;
-                    }
-                    else if (repetir == "N" || repetir == "n")
-                    {
-                        repeatProg = false;
-                        repeat = false;
-                    }
-                    else
-                    {
-                        repeat = true;
-                    }
+                    newG = NovaGramatica();
                 }
             }
         }
@@ -294,6 +136,146 @@ namespace Projeto1
             }
 
             return null;
+        }
+
+        public static int SelecionaOpcoes()
+        {
+            bool repeat = true;
+            int escolha = 0;
+
+            while (repeat)
+            {
+                Console.WriteLine("\nSelecione uma das opções abaixo:\n\n1 - Decifrar palavra\n2 - Derivar palavra");
+                escolha = System.Convert.ToInt32(Console.ReadLine());
+
+                if (escolha != 1 && escolha != 2)
+                {
+                    Console.WriteLine("\n\tERRO: Opção Inválida!");
+                    repeat = true;
+                }
+                else
+                {
+                    repeat = false;
+                }
+            }
+
+            return escolha;
+        }
+
+        public static bool NovaOperacao()
+        {
+            bool repeat = true;
+            string repetir = "";
+
+            while (repeat)
+            {
+                Console.WriteLine($"\nDeseja fazer uma nova operação? (S/N)");
+                repetir = Console.ReadLine();
+
+                if (repetir == "S" || repetir == "s")
+                {
+                    repeat = false;
+                    return true;
+                }
+                else if (repetir == "N" || repetir == "n")
+                {
+                    repeat = false;
+                    return false;
+                }
+                else
+                {
+                    repeat = true;
+                }
+            }
+            return false;
+        }
+
+        public static bool NovaGramatica()
+        {
+            bool repeat = true;
+            string repetir = "";
+
+            while (repeat)
+            {
+                Console.WriteLine($"\nDeseja fazer usar uma nova Gramatica? (S/N)");
+                repetir = Console.ReadLine();
+
+                if (repetir == "S" || repetir == "s")
+                {
+                    repeat = false;
+                    return true;
+                }
+                else if (repetir == "N" || repetir == "n")
+                {
+                    repeat = false;
+                    return false;
+                }
+                else
+                {
+                    repeat = true;
+                }
+            }
+            return false;
+        }
+
+        public static string GetPalavra()
+        {
+            string palavra = "";
+            bool repeat = true;
+            while (repeat)
+            {
+                Console.WriteLine("\nDigite a palavra a ser derivada.");
+                palavra = Console.ReadLine();
+
+                if (palavra == "")
+                {
+                    Console.WriteLine("\n\tERRO: Por favor, digite uma palavra!");
+                }
+                else
+                {
+                    repeat = false;
+                }
+            }
+            return palavra;
+        }
+
+        public static void GetEntradas(Grammar g, List<int> entrada)
+        {
+            bool repeat = true;
+
+            while (repeat)
+            {
+                Console.WriteLine("\nDigite os valores de entrada separados por vírgula. Ex: 1, 12, 7");
+                g.Aux = Console.ReadLine().Replace(" ", string.Empty).Split(',').ToList();
+
+                foreach (string s in g.Aux)
+                {
+                    if (s != "")
+                    {
+                        entrada.Add(Convert.ToInt32(s));
+                    }
+                }
+
+                if (entrada.Count <= 0)
+                {
+                    Console.WriteLine("\n\tERRO: Por favor, digite ao menos um valor de entrada!");
+                    entrada = new List<int>();
+                }
+                else if (entrada.Max() > g.P0.Count)
+                {
+                    Console.WriteLine("\n\tERRO: Por favor, digite apenas entradas válidas!");
+                    entrada = new List<int>();
+                }
+                else if (g.P0[entrada[0] - 1] != g.Inicial)
+                {
+                    Console.WriteLine("\n\tERRO: Por favor, a primeira entrada deve possuir a variável Inicial!");
+                    entrada = new List<int>();
+                }
+                else
+                {
+                    repeat = false;
+                }
+            }
         }
     }
 }
